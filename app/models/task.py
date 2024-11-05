ba7f0f706d49ac66143bb3a6565b8a077cf599e8
maxts <maksTS1991@yandex.ru>
2024-11-05 14:31:14 +0300

    Добавлено решение задачи "Модели SQLAlchemy"

---
+++ app/models/task.py
@@ -0,0 +1,25 @@
+from app.backend.db import Base
+from sqlalchemy import INTEGER, String, ForeignKey, Column, BOOLEAN
+from sqlalchemy.orm import relationship
+from app.models import *
+
+# from app.models.user import User
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'keep_existing': True}
+    id = Column(INTEGER, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(INTEGER, default=0)
+    completed = Column(BOOLEAN, default=False)
+    user_id = Column(INTEGER, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True)
+
+
+user = relationship('User', back_populates='tasks')
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(Task.__table__))
